2f8bb13411a8037aaa630de5a65e325a674cf14e
darckrow <ramonmateus30@gmail.com>
2025-03-06 16:08:43 -0300

    feat: -criação de modelo padrão para tabelas na page sheets -modelagem da page Home e estabeler modelo inicial (inda com dados ficticios)

--- src/pages/Sheets/Sheets.tsx
+++ src/pages/Sheets/Sheets.tsx
@@ -1,7 +1,60 @@
 import React from 'react'
-
+import { Table } from '@mui/joy';
 export default function Sheets() {
   return (
-    <div>Sheets</div>
+    <div>
+<Table aria-label="basic table" color='neutral' sx={styles}>
+      <thead>
+        <tr>
+          <th style={{ width: '40%' }}>Entrada de Valor (R$)</th>
+          <th>Valor</th>
+          <th>Data&nbsp;</th>
+          <th>Categoria&nbsp;</th>
+          <th>Observação&nbsp;</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Salario</td>
+          <td>R$ 2,000</td>
+          <td>03/03/2003</td>
+          <td>Entrada</td>
+          <td>Salario mês março</td>
+        </tr>
+        <tr>
+          <td>Ice cream sandwich</td>
+          <td>237</td>
+          <td>9</td>
+          <td>37</td>
+          <td>4.3</td>
+        </tr>
+        <tr>
+          <td>Eclair</td>
+          <td>262</td>
+          <td>16</td>
+          <td>24</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>Cupcake</td>
+          <td>305</td>
+          <td>3.7</td>
+          <td>67</td>
+          <td>4.3</td>
+        </tr>
+        <tr>
+          <td>Gingerbread</td>
+          <td>356</td>
+          <td>16</td>
+          <td>49</td>
+          <td>3.9</td>
+        </tr>
+      </tbody>
+    </Table>
+    </div>
   )
+}
+
+const styles = {
+  color: 'white',
 }
